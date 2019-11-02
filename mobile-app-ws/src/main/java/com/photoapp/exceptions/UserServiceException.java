1a21cf24172690ac28d1d2dbb5fb443a707d7815
smile <smile@10.12.1.229>
2019-11-02 22:35:16 +0300

    Exceptions Handling was added

---
+++ mobile-app-ws/src/main/java/com/photoapp/exceptions/UserServiceException.java
@@ -0,0 +1,14 @@
+package com.photoapp.exceptions;
+
+public class UserServiceException extends RuntimeException {
+	
+	private static final long serialVersionUID = -1355767568889918006L;
+
+	public UserServiceException(String message) {
+		super(message);
+		
+	}
+
+
+
+}
--- mobile-app-ws/src/main/java/com/photoapp/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/photoapp/ui/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.photoapp.ui.controller;
 
+
+
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.photoapp.exceptions.UserServiceException;
 import com.photoapp.service.UserService;
 import com.photoapp.shared.dto.UserDTO;
 import com.photoapp.ui.model.request.UserDetailsRequestModel;
+import com.photoapp.ui.model.response.ErrorMessages;
 import com.photoapp.ui.model.response.UserRest;
 
 @RestController
@@ -23,7 +29,9 @@ public class UserController {
 	@Autowired
 	UserService userService;
 	
-	@GetMapping(path = "/{id}")
+	@GetMapping(path = "/{id}",
+			produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+			)
 	public UserRest getUser(@PathVariable String id) {
 		UserRest returnValue = new UserRest();
 		UserDTO userDTO = userService.getUserByUserId(id);
@@ -31,10 +39,14 @@ public class UserController {
 		return returnValue;
 	}
 	
-	@PostMapping
-	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+	@PostMapping(
+			consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+			produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+			)
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception{
 		
 		UserRest returnValue = new UserRest();
+		if(userDetails.getFirstName().isEmpty()) throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
 		UserDTO userDTO = new UserDTO();
 		BeanUtils.copyProperties(userDetails, userDTO);
 		UserDTO createdUser = userService.createUser(userDTO);
